c83138ddfbbdd1051052401c4b47f8254d23903b
Anna Kova <m3ta007@gmail.com>
2019-05-01 09:21:43 -0400

    rewriting in BEM with Grid layout

--- scripts.js
+++ scripts.js
@@ -1,37 +1,46 @@
 // BS. Scrollspy
-$('body').scrollspy({ target: '#mainNavbar' })
+
+$('body').scrollspy({ target: '.nav' })
 
 $('[data-spy="scroll"]').each(function () {
     let $spy = $(this).scrollspy('refresh')
   })
 
+
 // BS. Navbar scrolling
+
 $(function () {
     $(document).scroll(function() {
-        let $nav = $('#mainNavbar');
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-        $nav.toggleClass('shadow', $(this).scrollTop() > $nav.height());
+        let $nav = $('.nav');
+        $nav.toggleClass('nav--scrolled', $(this).scrollTop() > $nav.height());
+        $nav.toggleClass('nav--shadow', $(this).scrollTop() > $nav.height());
     })
 })
 
+
 // Scroll Indicator (captures percentage)
 // - inspired by Bloomberg Article Scroll Indicator, a pen by Derek Palladino, published at: https://codepen.io/derekjp/pen/pJzpxB
+
 $(window).scroll(function(){
     let wintop = $(window).scrollTop(), docheight = 
         $(document).height(), winheight = $(window).height();
     let scrolled = (wintop/(docheight-winheight))*100;
 
-    $('.scroll-line').css('width', (scrolled + '%'));
+    $('.nav__scrolling').css('width', (scrolled + '%'));
 });
 
+
 // Hamburger Icon animation
+
 $(document).ready(function () {
-    $('.navbar-toggler').on('click', function () {
-        $('.animated-icon').toggleClass('open');
+    $('.nav__toggler').on('click', function () {
+        $('.nav__hamburger').toggleClass('.nav__hamburger--open');
     });
 });
 
+
 // Copyright Date Script
+
 let copyDates = document.querySelector('span#copyDate'),
     pubStr = document.querySelector('span#pubDate'),
     pubNum = parseInt(pubStr.textContent);
@@ -41,9 +50,14 @@ if (new Date().getFullYear()>pubNum) {
     copyDates.textContent = `-${currDate}`;
 }
 
+
 // Global site tag (gtag.js) - Google Analytics Script
+
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'UA-131142493-1');
+
+
+
